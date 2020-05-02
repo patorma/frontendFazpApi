6dbeda06a3088ed93e72310102c572a01aaf9236
patorma <pocontrera@ing.ucsc.cl>
2020-05-02 02:33:28 -0400

    se logro condicionar que al iniciar registro apareciera cerrar sesion y desapareciera iniciar  sesion y registrar

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,20 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "../../services/auth.service";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.css"],
 })
 export class HeaderComponent implements OnInit {
+  title: string = "NG SimpleAuth";
+  tasks: string = "Tasks";
+  private: string = "Private";
+  signin: string = "SignIn";
+  signup: string = "SignUp";
+  constructor(public authService: AuthService) {}
 
-  title: string = 'NG SimpleAuth';
-  tasks: string = 'Tasks';
-  private: string = 'Private';
-  signin: string =   'SignIn';
-  signup: string = 'SignUp';
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,27 +1,33 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Router } from "@angular/router";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class AuthService {
+  private URL = "http://localhost:3303/api";
+  constructor(private httpClient: HttpClient, private router: Router) {}
 
-  private URL = 'http://localhost:3303/api'
-  constructor(private httpClient: HttpClient) { }
-
-  signUp(user){
+  signUp(user) {
     // retorna un observable
     return this.httpClient.post<any>(`${this.URL}/signup`, user);
   }
 
-  signIn(user){
+  signIn(user) {
     return this.httpClient.post<any>(`${this.URL}/signin`, user);
   }
 
   // comprobar si el usuario esta logeado o no
-  loggedIn(){
+  loggedIn() {
     // si tiene el token esta logeado , sino no esta logueado
-    return !!localStorage.getItem('token');
+    return !!localStorage.getItem("token");
   }
-
+  logout() {
+    localStorage.removeItem("token");
+    this.router.navigate(["/tasks"]);
+  }
+  getToken() {
+    return localStorage.getItem("token");
+  }
 }
